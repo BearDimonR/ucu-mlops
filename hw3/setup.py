ad2bb76dbaa919a20d47dea9c159a7e26633fe62
Dmytro Miedviediev <beardimon2001@gmail.com>
2024-06-12 01:21:10 +0300

    add packages

--- hw3/setup.py
+++ hw3/setup.py
@@ -2,7 +2,10 @@ import setuptools
 
 setuptools.setup(
     install_requires=[
-        'google-cloud-aiplatform'
+        'google-cloud-aiplatform',
+        'google-cloud-bigquery'
+        'scikit-learn',
+        'joblib'
     ],
     packages=setuptools.find_packages()
 )
